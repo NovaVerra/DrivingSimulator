9a5b06dd3274eee9f34f79122147ed0e2d1270c6
lununglee <leo.lunung.lee@gmail.com>
2020-04-02 11:27:14 -0700

    modified file

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour
 {
 	/** Game Configuration */
 	[SerializeField] float	VehicleSpeed = 20.0f;
-	[SerializeField] float	TurnSpeed = 10.0f;
+	[SerializeField] float	TurnSpeed = 50.0f;
 	float	HorizontalInput;
 	float	VerticalInput;
 
@@ -18,10 +18,19 @@ public class PlayerController : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
+		GetPlayerInput();
+		MoveVehicle();
+	}
+
+	void	GetPlayerInput()
+	{
 		HorizontalInput = Input.GetAxis("Horizontal");
 		VerticalInput = Input.GetAxis("Vertical");
+	}
 
+	void	MoveVehicle()
+	{
 		transform.Translate(Vector3.forward * Time.deltaTime * VehicleSpeed * VerticalInput);
-		transform.Translate(Vector3.right * Time.deltaTime * TurnSpeed * HorizontalInput);
+		transform.Rotate(Vector3.up, Time.deltaTime * TurnSpeed * HorizontalInput);
 	}
 }
