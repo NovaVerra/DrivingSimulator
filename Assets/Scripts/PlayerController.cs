2f63eba36b74a1b1e97efa78d90a980d9c8cb553
lununglee <leo.lunung.lee@gmail.com>
2020-04-22 17:01:18 -0700

    shifted center of mass and added wheel colliders for realistic physics

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 	/** Game Configuration */
-	[SerializeField] float	VehicleSpeed = 20.0f;
+	// [SerializeField] float	VehicleSpeed = 20.0f;
+	[SerializeField] float	HorsePower = 50000.0f;
 	[SerializeField] float	TurnSpeed = 50.0f;
+	[SerializeField] GameObject	CenterOfMass;
 	float	HorizontalInput;
 	float	VerticalInput;
+	Rigidbody	RB_Player;
+
+	void	Start()
+	{
+		RB_Player = GetComponent<Rigidbody>();
+		RB_Player.centerOfMass = CenterOfMass.transform.position;
+	}
 
 	// Update is called once per frame
-	void FixedUpdate()
+	void	FixedUpdate()
 	{
 		GetPlayerInput();
 		MoveVehicle();
@@ -25,7 +34,8 @@ public class PlayerController : MonoBehaviour
 
 	void	MoveVehicle()
 	{
-		transform.Translate(Vector3.forward * Time.deltaTime * VehicleSpeed * VerticalInput);
+		// transform.Translate(Vector3.forward * Time.deltaTime * VehicleSpeed * VerticalInput);
+		RB_Player.AddRelativeForce(Vector3.forward * HorsePower * VerticalInput);
 		transform.Rotate(Vector3.up, Time.deltaTime * TurnSpeed * HorizontalInput);
 	}
 }
